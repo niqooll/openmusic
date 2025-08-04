6ef35d78e58e8c724dda757b6dc970ec7e92ca56
niqooll <niqozr01@gmail.com>
2025-08-04 15:45:15 +0700

    fixing v2 x3

--- migrations/1754291885583_enable-pg-trgm.js
+++ migrations/1754291885583_enable-pg-trgm.js
@@ -1,7 +1,9 @@
 /* eslint-disable camelcase */
 
 exports.up = (pgm) => {
-  pgm.createExtension('pg_trgm', { ifNotExists: true });
+  pgm.createExtension('pg_trgm', {
+    ifNotExists: true,
+  });
 };
 
 exports.down = (pgm) => {
